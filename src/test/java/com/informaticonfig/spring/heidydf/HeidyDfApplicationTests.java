872832eb123aff62509c1b86c508a295b9890118
JimmyDVO0823 <jimmyvelandia@outlook.es>
2025-12-22 14:41:08 -0500

    Primer commit del cursod e spring boot

---
+++ src/test/java/com/informaticonfig/spring/heidydf/HeidyDfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informaticonfig.spring.heidydf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeidyDfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
